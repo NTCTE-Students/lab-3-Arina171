9c314185fdb9f525534b4c6047b6fd4796f2f8f9
Ваше имя < >
2025-03-26 15:13:30 +0500

    лаба 3 сделана

---
+++ independets/work_10.php
@@ -0,0 +1,25 @@
+<?php
+$students=[
+    [
+        'name'=>'Алексей',
+        'age'=>22,
+    ],
+    [
+        'name'=>'Мария',
+        'age'=>20,
+    ],
+    [
+        'name'=>'Дмитрий',
+        'age'=>23,
+    ],
+];
+echo '<table border="1">';
+echo '<tr><th>Имя</th><th>Возраст</th></tr>';
+foreach ($students as $student) {
+    echo '<tr>';
+    echo '<td>' . $student['name'] . '</td>';
+    echo '<td>' . $student['age'] . '</td>';
+    echo '</tr>';
+}
+echo '</table>';
+?>
